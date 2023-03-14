57193ab5e58ec31bb6b9af316f1ca03efde965ff
Amine Bensalah <amine.bensalah@intech-consulting.fr>
2023-03-14 08:09:21 +0100

    feat: add new extension and content

--- Package.swift
+++ Package.swift
@@ -4,15 +4,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "core-extension",
+    name: "foundation-extension",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "CoreExtension",
-            targets: ["CoreExtension"]),
+            name: "FoundationExtension",
+            targets: ["FoundationExtension"]),
         .library(
-            name: "CoreTestExtension",
-            targets: ["CoreTestExtension"]),
+            name: "FoundationTestExtension",
+            targets: ["FoundationTestExtension"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -22,16 +22,16 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "CoreExtension",
+            name: "FoundationExtension",
             dependencies: []),
         .target(
-            name: "CoreTestExtension",
+            name: "FoundationTestExtension",
             dependencies: []),
         .testTarget(
-            name: "CoreExtensionTests",
-            dependencies: ["CoreExtension"]),
+            name: "FoundationExtensionTests",
+            dependencies: ["FoundationExtension"]),
         .testTarget(
-            name: "CoreTestExtensionTests",
-            dependencies: ["CoreTestExtension"]),
+            name: "FoundationTestExtensionTests",
+            dependencies: ["FoundationTestExtension"]),
     ]
 )
